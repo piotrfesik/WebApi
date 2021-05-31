2fb2b0dfe689786b6e68f7532b17010f294f076e
piotrfesik <petr.fesik@oxagile.com>
2021-05-31 12:04:09 +0300

    Fixed abstract Repo

--- WebApplication.Data/Repositories/BaseRepository/BaseRepository.cs
+++ WebApplication.Data/Repositories/BaseRepository/BaseRepository.cs
@@ -11,8 +11,6 @@ namespace WebApplication.Data.Repositories.BaseRepository
     public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         private ApplicationDbContext _context;
-        
-        private bool _isDisposed;
 
         protected BaseRepository(ApplicationDbContext context)
         {
@@ -30,11 +28,6 @@ namespace WebApplication.Data.Repositories.BaseRepository
             return _context.Set<T>().AsQueryable();
         }
 
-        public virtual async Task<IQueryable<T>> FindByIdAsync(int id)
-        {
-            return await Task.FromResult(_context.Set<T>().Where(x => x.GetType().GetProperty("Id").GetValue(x).ToString() == id.ToString()).AsQueryable());
-        }
-
         public virtual IQueryable<T> FindByCondition(Expression<Func<T, bool>> searchExpression)
         {
             return _context.Set<T>().Where(searchExpression).AsQueryable();
@@ -44,20 +37,6 @@ namespace WebApplication.Data.Repositories.BaseRepository
         {
             _context.Set<T>().Remove(entity);
         }
-
-
-        public virtual void Add(T entity)
-        {
-            try
-            {
-                _context.Set<T>().Add(entity);
-            }
-            catch (ApplicationException ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
 
         public virtual async Task AddAsync(T entity)
         {
@@ -71,45 +50,6 @@ namespace WebApplication.Data.Repositories.BaseRepository
             }
         }
 
-
-        public virtual void Update(T entity)
-        {
-            try
-            {
-                _context.Set<T>().Update(entity);
-            }
-            catch (ApplicationException ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-
-        public virtual void Save()
-        {
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (ApplicationException ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-        public virtual void Detached(T entity)
-        {
-            try
-            {
-                _context.Entry(entity).State = EntityState.Detached;
-            }
-            catch (ApplicationException ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-
         public virtual async Task SaveAsync()
         {
             try
@@ -120,19 +60,6 @@ namespace WebApplication.Data.Repositories.BaseRepository
             {
                 Debug.WriteLine(ex);
             }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        private void Dispose(bool isDisposing)
-        {
-            if (_isDisposed) return;
-            _isDisposed = true;
-            if (!isDisposing) return;
-            _context?.Dispose();
         }
     }
 }
--- WebApplication.Data/Repositories/BaseRepository/IBaseRepository.cs
+++ WebApplication.Data/Repositories/BaseRepository/IBaseRepository.cs
@@ -5,26 +5,16 @@ using System.Threading.Tasks;
 
 namespace WebApplication.Data.Repositories.BaseRepository
 {
-    public interface IBaseRepository<T> : IDisposable where T : class
+    public interface IBaseRepository<T>  where T : class
     {
         IQueryable<T> GetAll();
-
-        Task<IQueryable<T>> FindByIdAsync(int id);
 
         IQueryable<T> FindByCondition(Expression<Func<T, bool>> searchExpression);
 
         void Delete(T entity);
 
-        void Add(T entity);
-
         Task AddAsync(T entity);
 
-        void Update(T entity);
-
         Task SaveAsync();
-
-        void Save();
-
-        void Detached(T entity);
     }
 }
